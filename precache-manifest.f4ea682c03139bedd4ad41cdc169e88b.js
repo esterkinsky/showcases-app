5c329539f4fc8785325d3a2c7546cfad2288dfde
ester kinsky <esterkinsky@mail.ru>
2023-02-12 16:33:24 +0400

    Updates

---
+++ precache-manifest.f4ea682c03139bedd4ad41cdc169e88b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74b3320e9d5669d81106",
+    "url": "/showcases-app/static/css/main.fb4c7490.chunk.css"
+  },
+  {
+    "revision": "74b3320e9d5669d81106",
+    "url": "/showcases-app/static/js/main.74b3320e.chunk.js"
+  },
+  {
+    "revision": "e8f846181cdb494425c3",
+    "url": "/showcases-app/static/js/1.e8f84618.chunk.js"
+  },
+  {
+    "revision": "a044f2ed981928e0135b",
+    "url": "/showcases-app/static/js/runtime~main.a044f2ed.js"
+  },
+  {
+    "revision": "42e51e1bebdca89b0d53f4c776b301c1",
+    "url": "/showcases-app/index.html"
+  }
+];
